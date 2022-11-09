c197ef583fd33020073f5645e52c3e75ac269c43
nessa kodo <nessakodo@gmail.com>
2022-11-08 20:05:01 -0700

    adding lesson 10

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -2,16 +2,15 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 
 interface IMyERC20 {
     function mint(address to, uint256 amount) external;
 }
 
-contract TokenSale {
+contract TokenSale is IMyERC20 {
     uint256 public ratio;
     address public paymentToken;
-
+    // underscore helps us make a state variable
     constructor(uint256 _ratio, address _paymentToken) {
         ratio = _ratio;
         paymentToken = IMyERC20(_paymentToken);
@@ -21,6 +20,6 @@ contract TokenSale {
 
     function buyTokens() external payable {
         uint256 amountToBeMinted = msg.value / ratio;
-        paymentToken.mint(msg.sender, uint256 amountToBeMinted);
+        paymentToken.mint(msg.sender, amount);
     }
 }
